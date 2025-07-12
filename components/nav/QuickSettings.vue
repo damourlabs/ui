cba4074db124fdf6cf1d8a59db8f259a0044b91e
Christopher <129872252+damourChris@users.noreply.github.com>
2025-07-12 15:21:10 +0200

    refactor(components/nav): replace DropdownMenu with Popover in QuickSettings component

--- components/nav/QuickSettings.vue
+++ components/nav/QuickSettings.vue
@@ -6,8 +6,8 @@ const { isMobile } = useSidebar()
 <template>
   <SidebarMenu>
     <SidebarMenuItem>
-      <DropdownMenu>
-        <DropdownMenuTrigger as-child>
+      <Popover>
+        <PopoverTrigger as-child>
           <SidebarMenuButton size="lg"
             class="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground">
             <span>Quick Settings<div class="flex-1 grid text-sm text-left leading-tight">
@@ -15,13 +15,11 @@ const { isMobile } = useSidebar()
             </span>
           <Icon name="lucide:cog" size="1.5rem"/>
           </SidebarMenuButton>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent class="rounded-lg w-[--reka-dropdown-menu-trigger-width] min-w-56"
+        </PopoverTrigger>
+        <PopoverContent class="rounded-lg w-[--reka-dropdown-menu-trigger-width] min-w-56"
         :side="isMobile ? 'bottom' : 'right'" align="end" :side-offset="4">
-          <DropdownMenuItem as-child>
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
+        </PopoverContent>
+      </Popover>
     </SidebarMenuItem>
   </SidebarMenu>
 </template>
