0e4e49064944852e1bf629ce0670ea2b40eab854
Christopher <129872252+damourChris@users.noreply.github.com>
2025-07-19 12:36:39 +0200

    fix(composables/useFooter): initialize footerConfig with default credits and links

--- composables/useFooter.ts
+++ composables/useFooter.ts
@@ -2,7 +2,8 @@ import type { FooterProps } from "~ui/components/nav/Footer.vue";
 
 export const useFooter = () => {
   const footerConfig = useState<FooterProps>('footer-config', () => ({
-
+    credits: '',
+    links: [],
   }))
 
   function updateFooterConfig(newConfig: FooterProps) {
