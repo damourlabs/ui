962cef8eed4eaff6de08768fc263ebb2c86d73f0
Christopher <129872252+damourChris@users.noreply.github.com>
2025-07-29 11:18:39 +0200

    fix: add reports directory to vitest config file

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,43 +2,44 @@ import { defineConfig } from 'vitest/config'
 import { resolve } from 'path'
 
 export default defineConfig({
-    test: {
-        environment: 'node',
-        globals: true,
-        setupFiles: ['./test/setup.ts'],
-        include: ['**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-        exclude: [
-            '**/node_modules/**',
-            '**/dist/**',
-            '**/.nuxt/**',
-            '**/.output/**'
-        ],
-        coverage: {
-            provider: 'v8',
-            reporter: ['text', 'json', 'html'],
-            exclude: [
-                'coverage/**',
-                'dist/**',
-                'packages/*/test{,s}/**',
-                '**/*.d.ts',
-                'cypress/**',
-                'test{,s}/**',
-                'test{,-*}.{js,cjs,mjs,ts,tsx,jsx}',
-                '**/*{.,-}test.{js,cjs,mjs,ts,tsx,jsx}',
-                '**/*{.,-}spec.{js,cjs,mjs,ts,tsx,jsx}',
-                '**/__tests__/**',
-                '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
-                '**/.{eslint,mocha,prettier}rc.{js,cjs,yml}',
-                '**/{vitest,vite}.config.{js,ts}',
-                '**/form-test-example.ts'
-            ]
-        }
-    },
-    resolve: {
-        alias: {
-            '~ui': resolve(__dirname, '.'),
-            '~': resolve(__dirname, '.'),
-            '@': resolve(__dirname, '.')
-        }
+  test: {
+    environment: 'node',
+    globals: true,
+    setupFiles: ['./test/setup.ts'],
+    include: ['**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    exclude: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/.nuxt/**',
+      '**/.output/**'
+    ],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      reportsDirectory: './coverage',
+      exclude: [
+        'coverage/**',
+        'dist/**',
+        'packages/*/test{,s}/**',
+        '**/*.d.ts',
+        'cypress/**',
+        'test{,s}/**',
+        'test{,-*}.{js,cjs,mjs,ts,tsx,jsx}',
+        '**/*{.,-}test.{js,cjs,mjs,ts,tsx,jsx}',
+        '**/*{.,-}spec.{js,cjs,mjs,ts,tsx,jsx}',
+        '**/__tests__/**',
+        '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
+        '**/.{eslint,mocha,prettier}rc.{js,cjs,yml}',
+        '**/{vitest,vite}.config.{js,ts}',
+        '**/form-test-example.ts'
+      ]
     }
+  },
+  resolve: {
+    alias: {
+      '~ui': resolve(__dirname, '.'),
+      '~': resolve(__dirname, '.'),
+      '@': resolve(__dirname, '.')
+    }
+  }
 })
